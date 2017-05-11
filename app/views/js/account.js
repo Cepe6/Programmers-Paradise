c058ed443e477fc3a48e5cbed228eb0f6f5834ee
Cepe6 <vmladenov22@gmail.com>
2017-05-11 19:37:45 +0300

    Major i guess?

---
+++ app/views/js/account.js
@@ -0,0 +1,12 @@
+/**
+ * Created by cepe6 on 04.05.17.
+ */
+function showAccountForm() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("account-form").style.display = "block";
+}
+
+function closeAccountForm() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("account-form").style.display = "none";
+}

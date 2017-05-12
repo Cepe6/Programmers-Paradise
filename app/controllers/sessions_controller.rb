02c03aeed9e3429d791e4c23ee3be76f3756d128
Cepe6 <vmladenov22@gmail.com>
2017-05-12 22:53:33 +0300

    New commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     @header_picture = 'account.png'
 
     user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password_digest])
+    if user && user.password_digest == params[:password_digest]
       session[:user_id] = user.id
       redirect_to user
     else
